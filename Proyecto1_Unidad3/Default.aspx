b2b5e34d8c4e0a114e29888e833a1e4d924b44ce
Jualperez <juakochile@hotmail.com>
2021-06-16 21:05:45 -0400

    Actividad

---
+++ Proyecto1_Unidad3/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Proyecto1_Unidad3.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row mt-5">
+        <div class="col-12 col-md-6 col-lg-5 mx-auto">
+            <div class="card-header bg-danger text-white text-center">
+                <h5>Registrar Paciente</h5>
+
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label class="form-label" for="RutTxt">Rut</label>
+                    <asp:TextBox runat="server" ID="RutTxt" CssClass="form-control"></asp:TextBox>
+                </div>
+              
+                <div class="mb-3">
+                    <label class="form-label" for="NombreTxt">Nombre</label>
+                    <asp:TextBox runat="server" ID="NombreTxt" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="ApellidoPaternoTxt">Apellido Paterno</label>
+                    <asp:TextBox runat="server" ID="ApellidoPaternoTxt" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="EdadTxt">Edad</label>
+                    <asp:TextBox runat="server" ID="EdadTxt" CssClass="form-control"></asp:TextBox>
+                </div>
+                 <div class="mb-3">
+                    <label class="form-label" for="FechaTxt">Fecha de Nacimiento</label>
+                    <asp:TextBox runat="server" ID="FechaTxt" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="EstadoCivilTxt">Estado Civil</label>
+                    <asp:DropDownList runat="server" ID="EstadoCivilTxt">
+                        <asp:ListItem Value="1" Text="Soltero"></asp:ListItem>
+                        <asp:ListItem Value="2" Text="Casado"></asp:ListItem>
+                        <asp:ListItem Value="3" Text="Divorciado"></asp:ListItem>
+                        <asp:ListItem Value="4" Text="Viudo"></asp:ListItem>
+
+                    </asp:DropDownList>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="EstadoCivilTxt">Estado Civil</label>
+                    <asp:RadioButtonList runat="server" ID="RadioButtonList1">
+                        <asp:ListItem Value="1" Text="Masculino"></asp:ListItem>
+                        <asp:ListItem Value="2" Text="Femenino"></asp:ListItem>
+                        <asp:ListItem Value="3" Text="No-Binario"></asp:ListItem>
+ 
+
+                    </asp:RadioButtonList>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+</asp:Content>
---
+++ Proyecto1_Unidad3/Default.aspx.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Proyecto1_Unidad3 {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// Control RutTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox RutTxt;
+        
+        /// <summary>
+        /// Control NombreTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NombreTxt;
+        
+        /// <summary>
+        /// Control ApellidoPaternoTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ApellidoPaternoTxt;
+        
+        /// <summary>
+        /// Control EdadTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EdadTxt;
+        
+        /// <summary>
+        /// Control FechaTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox FechaTxt;
+        
+        /// <summary>
+        /// Control EstadoCivilTxt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList EstadoCivilTxt;
+        
+        /// <summary>
+        /// Control RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+    }
+}
